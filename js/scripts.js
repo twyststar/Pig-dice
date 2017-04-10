82fb853df59ad6339ab395177d845eef4659acea
Starlynne <lewis.start0905@gmail.com>
2017-04-09 17:46:39 -0700

    Add logic for computer player.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,21 @@ function Player(name, roll, score) {
   this.roll = roll;
   this.score = score;
 }
+var circuitPlaying = false;
+
 
 Player.prototype.rolling = function() {
  var playerRoll = Math.floor((Math.random() * 6) + 1);
  $(".currentRoll").text(playerRoll);
     if (playerRoll === 1) {
       alert("You got a '1'! Next Player!");
-      return this.roll = 0;
+      if(circuitPlaying === true){
+        compPlayer.compRolling();
+        return this.roll = 0;
+      } else {
+          return this.roll = 0;
+      }
+
     } else {
       this.roll = (this.roll + playerRoll);
       return this.roll;
@@ -22,13 +30,54 @@ Player.prototype.holding = function() {
   this.roll = 0;
   if(this.score >= 40){
     confirm("Congratulations! ");
-    if (true) {
-      location.reload();
-    }
+    location.reload();
+    return this.score;
+  }else if (circuitPlaying === true){
+    compPlayer.compRolling();
+    return this.score;
+  } else {
+    return this.score;
   }
-  return this.score;
+
 }
 
+
+Player.prototype.compRolling = function() {
+ var playerRoll = Math.floor((Math.random() * 6) + 1);
+ $(".currentRoll").text(playerRoll);
+    if (playerRoll === 1) {
+      alert("Circuit got a '1'! Your Turn " + onePlayer.name + "!");
+      this.roll = 0;
+      this.score = this.score + this.roll;
+      $(".compscore").text(this.score);
+      $(".comproll").text("");
+      $(".p2score").text(this.score);
+      // $("#p1button").prop("disabled", false);
+      // $(".p1hold").prop("disabled", false);
+      return this.score;
+    } else if(playerRoll >1 && this.roll < 7) {
+      this.roll = (this.roll + playerRoll);
+      $("comproll").text(this.roll);
+      compPlayer.compRolling();
+    }else {
+       this.score = this.roll + this.roll;
+       alert("Circuit holds! Your turn " + onePlayer.name + "!");
+       $(".compscore").text(this.score);
+       $(".p2score").text(this.score);
+          if (this.score > 40){
+            alert("Circuit wins! Try again!")
+          }
+       return this.score;
+}
+}
+// var toggleBtn = function(){
+//   if($("#p2button").prop("Disabled", true)){
+//     ($("#p2button").prop("Disabled", false));
+//   }
+//   if ($("button").prop("Disabled", false)){
+//     ($("button").prop("Disabled", true));
+//   }
+// }
 
 
 
@@ -41,6 +90,8 @@ $(document).ready(function(){
     onePlayer = new Player(pOneInput, 0, 0);
     $(".playerOneName").text(onePlayer.name);
     $(".playOneDiv").hide();
+    // $("#p2button").prop("disabled", true);
+    // $(".p2hold").prop("disabled", true);
   });
 
   $("#playerTwo").submit(function(event){
@@ -75,10 +126,9 @@ $(document).ready(function(){
     var oneScore = onePlayer.holding();
     $(".p1score").text(oneScore);
     $(".p1roll").text("");
-    $("#p1button").prop("disabled", true);
-    $(".p1hold").prop("disabled", true);
-    $("#p2button").prop("disabled", false);
-    $(".p2hold").prop("disabled", false);
+
+    // $("#p2button").prop("disabled", false);
+    // $(".p2hold").prop("disabled", false);
   });
 
   $(".p2hold").click(function(event){
@@ -87,10 +137,10 @@ $(document).ready(function(){
     var twoScore = twoPlayer.holding();
     $(".p2score").text(twoScore);
     $(".p2roll").text("");
-    $("#p2button").prop("disabled", true);
-    $(".p2hold").prop("disabled", true);
-    $("#p1button").prop("disabled", false);
-    $(".p1hold").prop("disabled", false);
+    // $("#p2button").prop("disabled", true);
+    // $(".p2hold").prop("disabled", true);
+    // $("#p1button").prop("disabled", false);
+    // $(".p1hold").prop("disabled", false);
   });
 
   $("#singlePlayer").click(function(event){
@@ -99,6 +149,7 @@ $(document).ready(function(){
     $("#playerTwo").hide();
     compPlayer = new Player("Circuit", 0, 0);
     $(".playerTwoName").text("Circuit");
+    circuitPlaying = true;
     console.log(compPlayer);
   })
 
